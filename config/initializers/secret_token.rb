13ef0af61adec3d9eca1087ede09d93e406440cc
Brian Ryckbost <bryckbost@gmail.com>
2012-01-28 14:52:17 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialMentions::Application.config.secret_token = 'bc15dd9fc9634d3600283fa4c3086929dd85cb426da73564f11ac55c39f8101ad592d9dfeb524116b63dee8c7defa35d10f127d1437c192e4064b55092690c3f'
